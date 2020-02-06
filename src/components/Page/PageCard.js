52541c9507c0cd7ad8ebb1786976c4c2ba4f45ae
hajczek <i.hajczewska@gmail.com>
2020-02-06 09:10:17 +0100

    Create scss files for every part of page

--- src/components/Page/PageCard.js
+++ src/components/Page/PageCard.js
@@ -6,7 +6,7 @@ import Footer from "./Footer";
 
 const PageCard = () => {
   return (
-    <div className="content">
+    <div className="contentPage">
       <Header />
       <Info />
       <MoreInfo />
