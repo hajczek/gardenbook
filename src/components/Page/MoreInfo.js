afc7444c58aaf52cd26ec4add8150f7c2e0d34c5
hajczek <i.hajczewska@gmail.com>
2020-02-06 10:13:43 +0100

    Added styles for more-info box

--- src/components/Page/MoreInfo.js
+++ src/components/Page/MoreInfo.js
@@ -3,12 +3,12 @@ import React from "react";
 const MoreInfo = () => {
   return (
     <div className="more-info">
-      <div className="tile1">Społeczność ogrodników</div>
-      <div className="tile2">Wymiana wiedzy</div>
-      <div className="tile3">Przydatne narzędzia</div>
-      <div className="tile4">Magazyn danych</div>
-      <div className="tile5">Baza firm ogrodniczych</div>
-      <div className="tile6">Wykonawca w pobliżu</div>
+      <div className="title">Społeczność ogrodników</div>
+      <div className="title">Wymiana wiedzy</div>
+      <div className="title">Przydatne narzędzia</div>
+      <div className="title">Magazyn danych</div>
+      <div className="title">Baza firm ogrodniczych</div>
+      <div className="title">Wykonawca w pobliżu</div>
     </div>
   );
 };
