10971c17b7a589f432b6af4459270c3177baabb8
hajczek <i.hajczewska@gmail.com>
2020-04-14 16:56:40 +0200

    Added comments about needed functionality in PlannedWorksBox component

--- src/components/Alerts/PlannedWorksBox.js
+++ src/components/Alerts/PlannedWorksBox.js
@@ -1,15 +1,19 @@
 import React from "react";
 
 const PlannedWorksBox = () => {
+  /* Needed functionality: 
+    1. display{PlannedWorksList() - display list of planned works (without these which are displays on alerts list!!!)
+       On list we must have links to card ith details about planned work.
+  */
   return (
     <div id="planned-works">
       <h2>ZAPLANOWANE PRACE</h2>
       <ol id="planned-works-list">
         <li>
-          Cięcie krzewów: <span>01-05-2020</span> &raquo;
+          Cięcie krzewów: <span>01-05-2020 &raquo;</span>
         </li>
         <li>
-          Prace porządkowe: <span>01-05-2020</span> &raquo;
+          Prace porządkowe: <span>01-05-2020 &raquo;</span>
         </li>
       </ol>
     </div>
