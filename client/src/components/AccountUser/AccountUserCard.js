4661efa7e602fedee12c07adc12e6ef4f56a9a2c
hajczek <i.hajczewska@gmail.com>
2020-04-24 15:46:09 +0200

    Remove all vendor prefixes from scss files

--- client/src/components/AccountUser/AccountUserCard.js
+++ client/src/components/AccountUser/AccountUserCard.js
@@ -1,16 +1,16 @@
 import React from "react";
 import AccountUserDataBox from "./AccountUserDataBox";
 import AccountUserFunctionsBox from "./AccountUserFunctionsBox";
-// import AccountUserDataEdit from "./AccountUserDataEdit";
-// import AccountUserFunctionsEdit from "./AccountUserFunctionsEdit";
+import AccountUserDataEdit from "./AccountUserDataEdit";
+import AccountUserFunctionsEdit from "./AccountUserFunctionsEdit";
 
 const AccountUserCard = () => {
   return (
     <div className="contentUser">
-      <AccountUserDataBox />
-      <AccountUserFunctionsBox />
-      {/* <AccountUserDataEdit />
-      <AccountUserFunctionsEdit /> */}
+      {/* <AccountUserDataBox />
+      <AccountUserFunctionsBox /> */}
+      <AccountUserDataEdit />
+      <AccountUserFunctionsEdit />
     </div>
   );
 };
