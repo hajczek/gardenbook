c87e7dad9a35c461d636c8ee6418130af3039a29
hajczek <i.hajczewska@gmail.com>
2020-04-25 11:40:25 +0200

    Added conditional statement for display home page or user pgae in App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "./context/GlobalState";
 import { GlobalProvider } from "./context/GlobalState";
 import PageCard from "./components/Page/PageCard";
-// import PageUser from "./components/User/PageUser";
+import PageUser from "./components/User/PageUser";
 import "./App.scss";
 
 const App = () => {
+  const { userData } = useContext(GlobalContext);
+
   return (
     <GlobalProvider>
       <div className="App">
-        <PageCard />
-        {/* <PageUser /> */}
+        {userData.map((data) =>
+          data.userLogged === false ? <PageCard /> : <PageUser />
+        )}
       </div>
     </GlobalProvider>
   );
