f9a2df76ab8accf6c938743add4161709f8bca37
hajczek <i.hajczewska@gmail.com>
2020-04-29 10:04:30 +0200

    Changed condition for display planned work on card with alerts

--- client/src/components/PlanWork/ExistedPlannedWorksBox.js
+++ client/src/components/PlanWork/ExistedPlannedWorksBox.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../../context/GlobalState";
+import addedDateFunction from "../../common/AddedDateFunction";
 
 const ExistedPlannedWorksBox = () => {
   const { plannedWorks } = useContext(GlobalContext);
@@ -7,12 +8,16 @@ const ExistedPlannedWorksBox = () => {
     <div className="existed-box">
       <h2>Zaplanowane</h2>
       <ol>
-        {plannedWorks.map((plannedWork) => (
-          <li key={plannedWork.id}>
-            {plannedWork.workName}:{" "}
-            <span className="greenText">{plannedWork.workTerm} &raquo;</span>
-          </li>
-        ))}
+        {plannedWorks
+          .filter((plannedWork) => plannedWork.workTerm >= addedDateFunction())
+          .map((plannedWork) => (
+            <li key={plannedWork.id}>
+              {plannedWork.workName}:{" "}
+              <span className="greenText">
+                {plannedWork.workTerm}, {plannedWork.workAlert} &raquo;
+              </span>
+            </li>
+          ))}
       </ol>
     </div>
   );
