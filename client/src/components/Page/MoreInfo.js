36e928b8a499333218a12d3baf88baefc927c8ee
hajczek <i.hajczewska@gmail.com>
2020-05-01 18:55:07 +0200

    Remove not needed div elements from MoreInfo component, changed definitions of css for these elements

--- client/src/components/Page/MoreInfo.js
+++ client/src/components/Page/MoreInfo.js
@@ -4,48 +4,32 @@ import { NavLink } from "react-router-dom";
 const MoreInfo = () => {
   return (
     <div className="more-info">
-      <div className="title">
-        <NavLink
-          to="/spolecznosc-ogrodnikow"
-          exact={true}
-          activeClassName="is-active"
-        >
-          Społeczność ogrodników
-        </NavLink>
-      </div>
-      <div className="title">
-        <NavLink to="/wymiana-wiedzy" exact={true} activeClassName="is-active">
-          Wymiana wiedzy
-        </NavLink>
-      </div>
-      <div className="title">
-        <NavLink
-          to="/przydatne-narzedzia"
-          exact={true}
-          activeClassName="is-active"
-        >
-          Przydatne narzędzia
-        </NavLink>
-      </div>
-      <div className="title">
-        <NavLink to="/magazyn-danych" exact={true} activeClassName="is-active">
-          Magazyn danych
-        </NavLink>
-      </div>
-      <div className="title">
-        <NavLink to="/plan-ogrodu" exact={true} activeClassName="is-active">
-          Plan Twojego ogrodu
-        </NavLink>
-      </div>
-      <div className="title">
-        <NavLink
-          to="/znajdz-wykonawce"
-          exact={true}
-          activeClassName="is-active"
-        >
-          Znajdź wykonawcę
-        </NavLink>
-      </div>
+      <NavLink
+        to="/spolecznosc-ogrodnikow"
+        exact={true}
+        activeClassName="is-active"
+      >
+        Społeczność ogrodników
+      </NavLink>
+      <NavLink to="/wymiana-wiedzy" exact={true} activeClassName="is-active">
+        Wymiana wiedzy
+      </NavLink>
+      <NavLink
+        to="/przydatne-narzedzia"
+        exact={true}
+        activeClassName="is-active"
+      >
+        Przydatne narzędzia
+      </NavLink>
+      <NavLink to="/magazyn-danych" exact={true} activeClassName="is-active">
+        Magazyn danych
+      </NavLink>
+      <NavLink to="/plan-ogrodu" exact={true} activeClassName="is-active">
+        Plan Twojego ogrodu
+      </NavLink>
+      <NavLink to="/znajdz-wykonawce" exact={true} activeClassName="is-active">
+        Znajdź wykonawcę
+      </NavLink>
     </div>
   );
 };
