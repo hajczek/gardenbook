cf74571114c0a45cec5d5fd762c0ce1b802f2d74
hajczek <i.hajczewska@gmail.com>
2020-05-04 09:44:56 +0200

    Changed csses for table with existed elements

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -12,7 +12,7 @@ const initialState = {
         workMatQuant: 0,
         workMatUnit: "",
       },
-      workTerm: "2020-05-03",
+      workTerm: "2020-05-07",
       workAlert: "09:00",
       workDetails: "Wykonać do południa",
       workDone: false,
