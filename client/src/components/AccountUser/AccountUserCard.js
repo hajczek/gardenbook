4776bd8b33a056858b888e8814d58c02cc92a067
hajczek <i.hajczewska@gmail.com>
2020-05-08 19:28:05 +0200

    Replace content from AccountUserFunctionsBox to AccountUserDataBox component

--- client/src/components/AccountUser/AccountUserCard.js
+++ client/src/components/AccountUser/AccountUserCard.js
@@ -1,12 +1,12 @@
 import React from "react";
 import AccountUserDataBox from "./AccountUserDataBox";
-import AccountUserFunctionsBox from "./AccountUserFunctionsBox";
+// import AccountUserFunctionsBox from "./AccountUserFunctionsBox";
 
 const AccountUserCard = () => {
   return (
     <div className="contentUser">
       <AccountUserDataBox />
-      <AccountUserFunctionsBox />
+      {/* <AccountUserFunctionsBox /> */}
     </div>
   );
 };
--- client/src/components/AccountUser/AccountUserDataBox.js
+++ client/src/components/AccountUser/AccountUserDataBox.js
@@ -11,27 +11,54 @@ const AccountUserDataBox = (props) => {
 
   return editData === false ? (
     <div className="user-data-box">
-      <h2>Twoje dane</h2>
+      <h2>Twoje dane:</h2>
       {userData.map((data) => (
-        <p key="user-data">
-          <span>Imię:</span> {data.userName}
-          <br />
-          <span>Email (login):</span> {data.userEmail}
-          <br />
-          {/* <span>Aktualne hasło:</span> {data.userPass}
+        <>
+          <p key="user-func">
+            <span>Imię:</span> {data.userName}
+            <br />
+            <span>Email (login):</span> {data.userEmail}
+            <br />
+            {/* <span>Aktualne hasło:</span> {data.userPass}
           <br /> */}
-          <span>Telefon:</span> {data.userTel}
-          <br />
-          <span>Lokalizacja:</span> {data.userPlace}
-          <br />
-          <button onClick={(e) => {
-              setEditData(true); 
-              setUserId(data.id);
-              }
-            }>
-            <FontAwesomeIcon id="edit-plant" icon={faEdit} /> Edytuj dane
-          </button>
-        </p>
+            <span>Telefon:</span> {data.userTel}
+            <br />
+            <span>Lokalizacja:</span> {data.userPlace}
+            <br />
+            <h2>Twoje ustawienia aplikacji:</h2>
+            <span>Planner prac: </span>
+            {data.accountSets.workPlanner === false ? "wył." : "wł."}
+            <br />
+            <span>Plan ogrodu: </span>
+            {data.accountSets.gardenPlan === false ? "wył." : "wł."}
+            <br />
+            <span>Statystyki: </span>
+            {data.accountSets.gardenStatistic === false ? "wył." : "wł."}
+            <br />
+            <span>Historia: </span>
+            {data.accountSets.gardenHistory === false ? "wył." : "wł."}
+            <br />
+            <span>Wyszukiwarka wykonawców: </span>
+            {data.accountSets.searchWorkers === false ? "wył." : "wł."}
+            <br />
+            <button
+              onClick={(e) => {
+                setEditData(true);
+                setUserId(data.id);
+              }}
+            >
+              <FontAwesomeIcon id="edit-plant" icon={faEdit} /> Zmień
+            </button>
+            {/* <button
+              onClick={(e) => {
+                setEditFunctionsOption(true);
+                setUserId(data.id);
+              }}
+            >
+              <FontAwesomeIcon id="edit-plant" icon={faEdit} /> Zmień ustawienia
+            </button> */}
+          </p>
+        </>
       ))}
     </div>
   ) : (
