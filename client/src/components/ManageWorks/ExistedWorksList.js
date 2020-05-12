bf88edb4c9542e11b4e59645c49e4a0e8f4185f5
hajczek <i.hajczewska@gmail.com>
2020-05-12 18:01:25 +0200

    Added function getWorks in ExistedWorksList component

--- client/src/components/ManageWorks/ExistedWorksList.js
+++ client/src/components/ManageWorks/ExistedWorksList.js
@@ -8,13 +8,18 @@ import ExistedWorksListHead from "./ExistedWorksListHead";
 import ExistedWorksSummary from "./ExistedWorksSummary";
 
 const PlannedWorkList = (props) => {
-  const { plannedWorks } = useContext(GlobalContext);
+  const { plannedWorks, getWorks } = useContext(GlobalContext);
   const { deleteWork } = useContext(GlobalContext);
   const [searchFrom, setSearchFrom] = useState("2020-01-01");
   const [searchTo, setSearchTo] = useState("2222-12-31");
   const [filteredWorks, setFilteredWorks] = useState([]);
   const [editWork, setEditWork] = useState(false);
   const [workId, setWorkId] = useState();
+
+  useEffect(() => {
+    getWorks();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
     setFilteredWorks(
@@ -28,97 +33,97 @@ const PlannedWorkList = (props) => {
 
   return editWork === false ? (
     <>
-    <p>Edytuj lub usuń wybrane zadania.</p>
-    <div className="contentList">
-      <div className="searchForWorks">
-        Wybierz okres od
-        <input
-          id="date-from"
-          type="date"
-          onChange={(e) => setSearchFrom(e.target.value)}
-        />
-        do
-        <input
-          type="date"
-          id="date-to"
-          onChange={(e) => setSearchTo(e.target.value)}
-        />
-      </div>
-      <table>
-        <tbody>
-          <ExistedWorksListHead />
+      <p>Edytuj lub usuń wybrane zadania.</p>
+      <div className="contentList">
+        <div className="searchForWorks">
+          Wybierz okres od
+          <input
+            id="date-from"
+            type="date"
+            onChange={(e) => setSearchFrom(e.target.value)}
+          />
+          do
+          <input
+            type="date"
+            id="date-to"
+            onChange={(e) => setSearchTo(e.target.value)}
+          />
+        </div>
+        <table>
+          <tbody>
+            <ExistedWorksListHead />
 
-          {filteredWorks.map((plannedWork) => (
-            <tr
-              key={plannedWork.id}
-              // Add special class name for done or not done work
-              className={
-                plannedWork.workDone === true
-                  ? "workDone"
-                  : plannedWork.workDone === false &&
-                    plannedWork.workTerm < addedDateFunction()
-                  ? "workNotDone"
-                  : plannedWork.workDone === false &&
-                    plannedWork.workTerm > addedDateFunction()
-                  ? ""
-                  : ""
-              }
-            >
-              <td id="work-id" align="center">
-                {plannedWork.id}
-              </td>
-              <td id="work-name">{plannedWork.workName}</td>
-              <td id="work-term" align="center">
-                {plannedWork.workTerm}
-              </td>
-              <td id="work-alert" align="center">
-                {plannedWork.workAlert}
-              </td>
-              <td id="work-done" align="center" className="center">
-                {plannedWork.workDone === false ? "-" : "+"}
-              </td>
-              <td id="work-time" align="center">
-                {plannedWork.workTime}
-              </td>
-              <td id="work-value" align="right">
-                {plannedWork.workValue.toFixed(2)}
-              </td>
-              <td align="center">
-                <span id="work-mat-name">
-                  {plannedWork.workMaterial.workMatName}
-                </span>
-                <span id="work-mat-quant" className="mat">
-                  {plannedWork.workMaterial.workMatQuant}
-                </span>
-                <span id="work-mat-unit">
-                  {plannedWork.workMaterial.workMatUnit}
-                </span>
-              </td>
-              <td id="work-details">{plannedWork.workDetails}</td>
-              <td align="center">
-                <FontAwesomeIcon
-                  id="edit-work"
-                  icon={faEdit}
-                  // This action opens EditedWork component and set work data to edit form
-                  onClick={(e) => {
-                    setEditWork(true);
-                    setWorkId(plannedWork.id);
-                  }}
-                />
-              </td>
-              <td align="center">
-                <FontAwesomeIcon
-                  id="delete-work"
-                  icon={faTrash}
-                  onClick={() => deleteWork(plannedWork.id)}
-                />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <ExistedWorksSummary />
-    </div>
+            {filteredWorks.map((plannedWork) => (
+              <tr
+                key={plannedWork.id}
+                // Add special class name for done or not done work
+                className={
+                  plannedWork.workDone === true
+                    ? "workDone"
+                    : plannedWork.workDone === false &&
+                      plannedWork.workTerm < addedDateFunction()
+                    ? "workNotDone"
+                    : plannedWork.workDone === false &&
+                      plannedWork.workTerm > addedDateFunction()
+                    ? ""
+                    : ""
+                }
+              >
+                <td id="work-id" align="center">
+                  {plannedWork.id}
+                </td>
+                <td id="work-name">{plannedWork.workName}</td>
+                <td id="work-term" align="center">
+                  {plannedWork.workTerm}
+                </td>
+                <td id="work-alert" align="center">
+                  {plannedWork.workAlert}
+                </td>
+                <td id="work-done" align="center" className="center">
+                  {plannedWork.workDone === false ? "-" : "+"}
+                </td>
+                <td id="work-time" align="center">
+                  {plannedWork.workTime}
+                </td>
+                <td id="work-value" align="right">
+                  {plannedWork.workValue.toFixed(2)}
+                </td>
+                <td align="center">
+                  <span id="work-mat-name">
+                    {plannedWork.workMaterial.workMatName}
+                  </span>
+                  <span id="work-mat-quant" className="mat">
+                    {plannedWork.workMaterial.workMatQuant}
+                  </span>
+                  <span id="work-mat-unit">
+                    {plannedWork.workMaterial.workMatUnit}
+                  </span>
+                </td>
+                <td id="work-details">{plannedWork.workDetails}</td>
+                <td align="center">
+                  <FontAwesomeIcon
+                    id="edit-work"
+                    icon={faEdit}
+                    // This action opens EditedWork component and set work data to edit form
+                    onClick={(e) => {
+                      setEditWork(true);
+                      setWorkId(plannedWork.id);
+                    }}
+                  />
+                </td>
+                <td align="center">
+                  <FontAwesomeIcon
+                    id="delete-work"
+                    icon={faTrash}
+                    onClick={() => deleteWork(plannedWork.id)}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <ExistedWorksSummary />
+      </div>
     </>
   ) : (
     <EditedWork workid={workId} />
