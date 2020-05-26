50b8c9e3228b02a478bff3363900269e4a61e0a6
hajczek <i.hajczewska@gmail.com>
2020-05-26 10:54:47 +0200

    Updates

---
+++ precache-manifest.403b9966c81e79b6eab0ae5aea3499e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f310644624919f363cd4ee9d5a0eb35",
+    "url": "/gardenbook/index.html"
+  },
+  {
+    "revision": "661355ab7828499c367c",
+    "url": "/gardenbook/static/css/main.a65a2065.chunk.css"
+  },
+  {
+    "revision": "ffa078abf6a0fb1891df",
+    "url": "/gardenbook/static/js/2.27481836.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/gardenbook/static/js/2.27481836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661355ab7828499c367c",
+    "url": "/gardenbook/static/js/main.9b487eec.chunk.js"
+  },
+  {
+    "revision": "8d42dedc29a48c7e18ff",
+    "url": "/gardenbook/static/js/runtime-main.7d44664c.js"
+  },
+  {
+    "revision": "6f06ab4ff04b486c1f5ff559924a3d23",
+    "url": "/gardenbook/static/media/gardenbook-bg.6f06ab4f.jpg"
+  }
+]);
