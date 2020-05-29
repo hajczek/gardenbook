7df6753cf12271ea13ab0b568fb5ae5a1757dea0
hajczek <i.hajczewska@gmail.com>
2020-05-29 17:14:55 +0200

    Remove not needed useState element from components App and HeaderUser

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { GlobalProvider } from "./context/GlobalState";
 import SectionToDisplay from "./common/SectionToDisplay";
--- client/src/components/User/HeaderUser.js
+++ client/src/components/User/HeaderUser.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { GlobalContext } from "../../context/GlobalState";
 import { NavLink } from "react-router-dom";
 import logo from "../../assets/img/gardenbook-logo.png";
