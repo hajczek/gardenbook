693ab926dad9c11ef5fb7368d51e0e2d750b2bb2
hajczek <i.hajczewska@gmail.com>
2020-05-29 17:21:23 +0200

    Remove not needed commented part of code

--- client/src/components/AccountUser/AccountUserCard.js
+++ client/src/components/AccountUser/AccountUserCard.js
@@ -1,12 +1,10 @@
 import React from "react";
 import AccountUserDataBox from "./AccountUserDataBox";
-// import AccountUserFunctionsBox from "./AccountUserFunctionsBox";
 
 const AccountUserCard = () => {
   return (
     <div className="user-content">
       <AccountUserDataBox />
-      {/* <AccountUserFunctionsBox /> */}
     </div>
   );
 };
