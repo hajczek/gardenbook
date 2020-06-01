a85a9d2434756a9cc8f05ceb6b88f087527ce83c
hajczek <i.hajczewska@gmail.com>
2020-06-01 10:06:54 +0200

    Added filter by user id for existed elements on database

--- client/src/components/AddPlant/ExistedPlantsBox.js
+++ client/src/components/AddPlant/ExistedPlantsBox.js
@@ -3,31 +3,37 @@ import { NavLink } from "react-router-dom";
 import { GlobalContext } from "../../context/GlobalState";
 
 const ExistedPlantsBox = () => {
-  const { plants, getPlants } = useContext(GlobalContext);
+  const { plants, getPlants, users } = useContext(GlobalContext);
 
   useEffect(() => {
     getPlants();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const userId = users
+    .filter((user) => user.userLogged === true)
+    .map((user) => user._id)[0];
+
   return (
     <div className="existed-box">
       <h2>ISTNIEJĄCE ROŚLINY</h2>
       <ol>
-        {plants.map((plant) => (
-          <li key={plant.id}>
-            <NavLink
-              to="/zarzadzaj-roslinami"
-              title="Zarządzaj roslinami"
-              exact={true}
-              activeClassName="is-active"
-              id="plants"
-              className="greenText"
-            >
-              {plant.plantName}: {plant.plantQuant} szt. &raquo;
-            </NavLink>
-          </li>
-        ))}
+        {plants
+          .filter((plant) => plant.userId === userId)
+          .map((plant) => (
+            <li key={plant.id}>
+              <NavLink
+                to="/zarzadzaj-roslinami"
+                title="Zarządzaj roslinami"
+                exact={true}
+                activeClassName="is-active"
+                id="plants"
+                className="greenText"
+              >
+                {plant.plantName}: {plant.plantQuant} szt. &raquo;
+              </NavLink>
+            </li>
+          ))}
       </ol>
     </div>
   );
--- client/src/components/PlanWork/ExistedPlannedWorksBox.js
+++ client/src/components/PlanWork/ExistedPlannedWorksBox.js
@@ -4,21 +4,29 @@ import { GlobalContext } from "../../context/GlobalState";
 import addedDateFunction from "../../common/AddedDateFunction";
 
 const ExistedPlannedWorksBox = () => {
-  const { works, getWorks } = useContext(GlobalContext);
+  const { works, getWorks, users } = useContext(GlobalContext);
 
   useEffect(() => {
     getWorks();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  console.log(works, addedDateFunction());
+  const userId = users
+    .filter((user) => user.userLogged === true)
+    .map((user) => user._id)[0];
+
+  console.log(userId);
 
   return (
     <div className="existed-box">
       <h2>Zaplanowane</h2>
       <ol>
         {works
-          .filter((plannedWork) => plannedWork.workTerm >= addedDateFunction())
+          .filter(
+            (plannedWork) =>
+              plannedWork.userId === userId &&
+              plannedWork.workTerm >= addedDateFunction()
+          )
           .map((plannedWork) => (
             <li key={plannedWork.id}>
               {plannedWork.workName}:{" "}
