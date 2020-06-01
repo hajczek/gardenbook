5a8d61446debc482e6bc12509ec088403f730fdd
hajczek <i.hajczewska@gmail.com>
2020-06-01 10:54:38 +0200

    Added option to filtered materials by user id in ExistedMaterialsSummary component

--- client/src/components/ManageMaterials/ExistedMaterialsSummary.js
+++ client/src/components/ManageMaterials/ExistedMaterialsSummary.js
@@ -2,20 +2,36 @@ import React, { useContext } from "react";
 import { GlobalContext } from "../../context/GlobalState";
 
 const ExistedMaterialsSummary = () => {
-  const { materials } = useContext(GlobalContext);
+  const { materials, users } = useContext(GlobalContext);
+
+  const userId = users
+    .filter((user) => user.userLogged === true)
+    .map((user) => user._id)[0];
 
   function countValueOfAllMaterials() {
     let valueOfAllMaterials = 0;
     for (let i = 0; i < materials.length; i++) {
-      valueOfAllMaterials +=
-        materials[i].materialQuant * materials[i].materialPrice;
+      if (materials[i].userId === userId) {
+        valueOfAllMaterials +=
+          materials[i].materialQuant * materials[i].materialPrice;
+      }
     }
     return valueOfAllMaterials.toFixed(2);
   }
 
+  function countNumOfMaterials() {
+    let numOfMaterials = 0;
+    for (let i = 0; i < materials.length; i++) {
+      if (materials[i].userId === userId) {
+        numOfMaterials += 1;
+      }
+    }
+    return numOfMaterials;
+  }
+
   return (
     <div className="summaryBox">
-      <h3>RAZEM:</h3> Il. materiałów: {materials.length} | Wartość:{" "}
+      <h3>RAZEM:</h3> Il. materiałów: {countNumOfMaterials()} | Wartość:{" "}
       {countValueOfAllMaterials()} zł
     </div>
   );
