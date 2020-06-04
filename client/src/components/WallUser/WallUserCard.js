abf096faddbab11660ad6b9ddc8a5a0898374e32
hajczek <i.hajczewska@gmail.com>
2020-06-04 10:58:54 +0200

    Remove footer from WallUserCard component

--- client/src/components/WallUser/WallUserCard.js
+++ client/src/components/WallUser/WallUserCard.js
@@ -1,14 +1,12 @@
 import React from "react";
 import RightBoxUser from "./RightBox/RightBoxUser";
 import LeftBoxUser from "./LeftBox/LeftBoxUser";
-import Footer from "../Page/Footer";
 
 const WallUser = () => {
   return (
     <div className="user-content">
       <LeftBoxUser />
       <RightBoxUser />
-      <Footer />
     </div>
   );
 };
