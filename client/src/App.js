d7267fdbd4ad3dae660f35be05d4fd41aba890c2
hajczek <i.hajczewska@gmail.com>
2020-09-29 18:56:27 +0200

    Added provider with setting default language for app

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import { GlobalProvider } from "./context/GlobalState";
-import SectionToDisplay from "./common/SectionToDisplay";
-import "./App.scss";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { GlobalProvider } from './context/GlobalState';
+import SectionToDisplay from './common/SectionToDisplay';
+import './App.scss';
+
+import { I18nProvider, LOCALES } from './i18n';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <GlobalProvider>
-        <div className="App">
-          <SectionToDisplay />
-        </div>
-      </GlobalProvider>
-    </BrowserRouter>
+    <I18nProvider locale={LOCALES.POLISH}>
+      <BrowserRouter>
+        <GlobalProvider>
+          <div className="App">
+            <SectionToDisplay />
+          </div>
+        </GlobalProvider>
+      </BrowserRouter>
+    </I18nProvider>
   );
 };
 
