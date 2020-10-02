3abdb3a2bb0170e9942bb2af8924c00fad6e7e54
hajczek <i.hajczewska@gmail.com>
2020-10-02 09:31:24 +0200

    Added terms for en and pl languages for main, login and register pages

--- client/src/i18n/messages/en-US.js
+++ client/src/i18n/messages/en-US.js
@@ -1,7 +1,54 @@
-import { LOCALES } from '../locales';
+import { LOCALES } from "../locales";
 
 export default {
   [LOCALES.ENGLISH]: {
-    'portal-title': 'gardeners social network',
+    /* MENU */
+    "home":"Home",
+    "register": "Registration",
+    "login":"Login",
+    "regulations": "Regulations",
+    "copyright": "gardenbook © 2020"
+    /***************** MAIN PAGE *****************/
+    "enables": "enables",
+    "allows": "allows",
+    "provides": "provides",
+    "enables-text": "easy exchange of knowledge and experience",
+    "allows-text": "to establish contacts with people from the industry",
+    "provides-text": "tools useful in garden management ",
+    "all-in-one": "All in one place!"
+    "create-an-acconut": "Create an account! Use gardenbook resources and tools.",
+    "btn-title-first": "Gardeners community",
+    "btn-title-second": "Useful tools",
+    "btn-title-third": "Data-storage",
+    "btn-title-fourth": "Active plan",
+    "btn-title-fifth": "Contractors nearby",
+    "btn-title-sixth": "History of changes",
+    /* FUNCTIONS */
+    "function":"function",
+    "function-gardeners-community-title": "",
+    "function-gardeners-community-text": "",
+    "function-useful-tools-title": "Useful tools",
+    "function-useful-tools-text": "grdenbook offers its users a set of garden management tools, inc work planner, alerts, statistics, active garden plan, history of changes, plant base and materials. All tools are available from within the account. To be able to use the selected tool, just activate it in the user panel.",
+    "function-data-storage-title": "",
+    "function-data-storage-text": "",
+    "function-active-plan-title": "",
+    "function-active-plan-text": "",
+    "function-contractors-nearby-title": "",
+    "function-contractors-nearby-text": "",
+    "function-history-title": "",
+    "function-history-text": "",
+    /***************** LOGIN PAGE *****************/
+    "login-panel": "Login Panel",
+    "login":"Login",
+    "password":"Password",
+    /***************** REGISTRATION PAGE *****************/
+    "register-panel":"Registration Panel",
+    "name":"Name",
+    "email":"E-mail",
+    "password": "Password",
+    "repeat-passwors":"Repeat password",
+    "required-fields":"required fields",
+    "register":"Register"
+    /***************** ALERTS PAGE *****************/
   },
 };
--- client/src/i18n/messages/pl-PL.js
+++ client/src/i18n/messages/pl-PL.js
@@ -1,7 +1,55 @@
-import { LOCALES } from '../locales';
+import { LOCALES } from "../locales";
 
 export default {
   [LOCALES.POLISH]: {
-    'portal-title': 'ogrodniczy serwis społecznościowy',
+    /* MENU */
+    "home":"",
+    "register": "",
+    "login":"",
+    "regulations": "",
+    "copyright": "gardenbook © 2020"
+    /***************** MAIN PAGE *****************/
+    "portal-title": "ogrodniczy serwis społecznościowy",
+    "enables": "umożliwia",
+    "allows": "ułatwia",
+    "provides": "udostępnia",
+    "enables-text": "łatwą wymianę wiedzy i doświadczeń",
+    "allows-text": "nawiązanie kontaktów z innymi ogrodnikami",
+    "provides-text": "narzędzia przydatne w zarządzaniu ogrodem",
+    "all-in-one": "Wszystko w jednym miejscu!"
+    "create-an-acconut": "Założ konto! Korzystaj z zasobów i narzędzi gardenbook'a.",
+    "btn-title-first": "Społeczność ogrodników",
+    "btn-title-second": "Przydatne narzędzia",
+    "btn-title-third": "Baza danych",
+    "btn-title-fourth": "Projekt ogrodu",
+    "btn-title-fifth": "Znajdź wykonawcę",
+    "btn-title-sixth": "Historia zmian",
+    /* FUNCTIONS */
+    "function":"Funckja",
+    "function-gardeners-community-title": "Społeczność ogrodników",
+    "function-gardeners-community-text": "Dzięki wbudowanemu w aplikację portalowi społecznościowemu użytkownicy będą mogli w łatwy sposób wymieniać się wiedzą, doswiadczeniem i opiniami. Ta funkcjonalność nie jest jeszcze wdrożona.",
+    "function-useful-tools-title": "Przydatne narzędzia",
+    "function-useful-tools-text": "gardenbook oferuje swoim użytkownikom wiele przydatnych narzędzi ułatwiających zarządzanie ogrodem i organizację niezbędnych prac. Należą do nich: planer prac, baza roślin i materiałów oraz statystyki. Planowane jest wdrożenie kolejnych: historia zmian, plan ogrodu oraz wyszukiwarka wykonawców prac ogrodniczych świadczących usługi w pobliżu lokalizacji użytkownika.",
+    "function-data-storage-title": "Baza danych",
+    "function-data-storage-text": "gardenbook oferuje swoim użytkownikom gromadzenie danych dotyczących ilości roślin i materiałów znajdujących się na stanie ogrodu. Umożliwia to łatwe zarzdządzanie posiadanymi zasobami.",
+    "function-active-plan-title": "Projekt ogrodu",
+    "function-active-plan-text": "Funkjonalność uumożliwiająca użytkownikom aplikacji łatwe zaprojektowanie ogrodu lub jego części. gardenbook oferuje proste narzędzia do sporządzania schematycznych projektów. Ta funkcjonalość nie jest jeszcze wdrożona.",
+    "function-contractors-nearby-title": "Znajdź wykonawcę",
+    "function-contractors-nearby-text": "Funkcjonalność ta umożliwi użytkownikom gardenbook'a wyszukiwanie wykonawców interesujących ich prac ogrodniczych w pobliżu ich lokalizacji. Ta funkcjonalność nie jest jeszcze wdrożona.",
+    "function-history-title": "Historia zmian",
+    "function-history-text": "gardenbook umożliwi gromadzenie wprowadzanych w aplikacji zmian. Dzięki temu użytkownik będzie miał wgląd w historię swoich działań. Ta funkcjonalność nie jest jeszcze wdrożona.",
+    /***************** LOGIN PAGE *****************/
+    "login-panel": "",
+    "login":"",
+    "password":"",
+    /***************** REGISTRATION PAGE *****************/
+    "register-panel":"",
+    "name":"",
+    "email":"",
+    "password":"",
+    "repeat-passwors":"",
+    "required-fields":"",
+    "register":""
+    /***************** ALERTS PAGE *****************/
   },
 };
