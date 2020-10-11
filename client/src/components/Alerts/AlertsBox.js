c2fea6ea6ff7b2949b69c8624de5878b5291ea39
hajczek <i.hajczewska@gmail.com>
2020-10-11 12:02:49 +0200

    Added new terms for alerts page

--- client/src/components/Alerts/AlertsBox.js
+++ client/src/components/Alerts/AlertsBox.js
@@ -1,12 +1,13 @@
-import React, { useContext, useEffect } from "react";
-import { GlobalContext } from "../../context/GlobalState";
-import { NavLink } from "react-router-dom";
-import { maxDateFormatted } from "./AlertsFunction";
-import CountAlerts from "./CountAlerts";
-import daysToAlert from "./CountDaysToAlert";
-import addedDateFunction from "../../common/AddedDateFunction";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBell } from "@fortawesome/free-solid-svg-icons";
+import React, { useContext, useEffect } from 'react';
+import { GlobalContext } from '../../context/GlobalState';
+import { NavLink } from 'react-router-dom';
+import { maxDateFormatted } from './AlertsFunction';
+import CountAlerts from './CountAlerts';
+import daysToAlert from './CountDaysToAlert';
+import addedDateFunction from '../../common/AddedDateFunction';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
+import translate from '../../i18n/translate';
 
 const AlertsBox = () => {
   const { works, getWorks, users } = useContext(GlobalContext);
@@ -20,10 +21,13 @@ const AlertsBox = () => {
     .filter((user) => user.userLogged === true)
     .map((user) => user._id)[0];
 
+  console.log(translate());
+
   return (
     <div id="alerts">
       <h2>
-        <FontAwesomeIcon icon={faBell} /> Aktualne alerty: <CountAlerts /> !!!
+        <FontAwesomeIcon icon={faBell} /> {translate('actual-alerts')}{' '}
+        <CountAlerts /> !!!
       </h2>
       <ol id="alerts-list">
         {works
@@ -37,16 +41,21 @@ const AlertsBox = () => {
           .map((alert) => {
             return (
               <li key={alert.id}>
-                {alert.workName}:
+                '{alert.workName}'{' '}
+                <span class="thin-span">
+                  {translate('in-term')} {daysToAlert(alert.workTerm)}{' '}
+                  {translate('days-term')}:{' '}
+                </span>
                 <NavLink
                   to="/zarzadzaj-pracami"
                   title="Zarządzaj pracami"
                   exact={true}
                   className={
-                    alert.workTerm === addedDateFunction() ? "redAlert" : null
+                    alert.workTerm === addedDateFunction() ? 'redAlert' : null
                   }
                 >
-                  {daysToAlert(alert.workTerm) > 1
+                  {alert.workTerm} {translate('at-term')} {alert.workAlert}{' '}
+                  {/* {daysToAlert(alert.workTerm) > 1
                     ? ` za ${daysToAlert(alert.workTerm)} dni o ${
                         alert.workAlert
                       } `
@@ -54,7 +63,7 @@ const AlertsBox = () => {
                     ? ` jutro o ${alert.workAlert} `
                     : daysToAlert(alert.workTerm) === 0
                     ? ` dzisiaj o ${alert.workAlert} `
-                    : null}
+                    : null} */}
                   &raquo;
                 </NavLink>
               </li>
