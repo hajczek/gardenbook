c27f860e3807147e2b2d4c7b86129f29614f3663
hajczek <i.hajczewska@gmail.com>
2020-10-30 16:43:25 +0100

    Changed structure of code and css for help page

--- client/src/components/User/Help.js
+++ client/src/components/User/Help.js
@@ -1,12 +1,14 @@
-import React from "react";
-import translate from "../../i18n/translate";
+import React from 'react';
+import translate from '../../i18n/translate';
 
 const Help = () => {
   return (
     <div className="user-content">
       <div className="user-right-box">
-        <h2>{translate("help-term")}</h2>
-        <p>{translate("help-text")}</p>
+        <div className="scroll-box">
+          <h2>{translate('help-term')}</h2>
+          <p>{translate('help-text')}</p>
+        </div>
       </div>
     </div>
   );
